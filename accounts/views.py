0a52a0976413d8f566bfa61c5bf07e4d5f211de6
JakubJas1997 <j.j4sinski@gmail.com>
2023-07-23 15:00:31 +0200

    Added GET method registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .forms import MyRegistrationForm
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'GET':
+        form = MyRegistrationForm()
+        return render(request, 'register.html', {'form': form})
