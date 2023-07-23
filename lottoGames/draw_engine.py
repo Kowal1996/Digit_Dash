6867e1f3de14a1037783e3e7f294f6f740142c00
KowalCapo <sk.kowalski9@gmail.com>
2023-07-23 15:43:33 +0200

    Added time tick module to draw_engine.py

--- lottoGames/draw_engine.py
+++ lottoGames/draw_engine.py
@@ -1,13 +1,17 @@
 from random import randint
+import time
 
 draw_result = []
 
-while len(draw_result)<4:
-    drawed_number = randint(1,39)
-    if not drawed_number in draw_result:
-        draw_result.append(drawed_number)
+def Draw_numbers():
+    while len(draw_result)<4:
+        drawed_number = randint(1,39)
+        if not drawed_number in draw_result:
+            draw_result.append(drawed_number)
+    return draw_result.sort()
 
-
-result_sorted = sorted(draw_result)
-
-print(result_sorted)
+while True:
+    Draw_numbers()
+    print(draw_result)
+    time.sleep(5)
+    draw_result = []
