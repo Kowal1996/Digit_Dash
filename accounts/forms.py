81cd75f6c67dc88d30262a96c34f21540bb703e8
JakubJas1997 <j.j4sinski@gmail.com>
2023-07-25 19:32:58 +0200

    Added Profile Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class MyRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -11,3 +12,9 @@ class MyRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('city', 'country', 'birth_date', 'account_balance', 'pictures')
