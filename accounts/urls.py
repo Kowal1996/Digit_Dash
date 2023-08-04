f7ac8d27bc180e6247751e250db8d44ec2803b9e
kamila <czajkowska.kamila@wp.pl>
2023-08-04 13:45:47 +0200

    password change view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('logoutUser/', views.logoutUser, name='logoutUser'),
     path('profileInformation/', views.profileInformation, name='profileInformation'),
     path('editProfile', views.editProfile, name='editProfile'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
 ]
