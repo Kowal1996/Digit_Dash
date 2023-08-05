017994de2c08f35b5adc4726c2a46cd2190faeee
kamila <czajkowska.kamila@wp.pl>
2023-08-05 14:57:44 +0200

    Added game model

--- lottoGames/admin.py
+++ lottoGames/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import OneOutOfTwenty
 
-# Register your models here.
+# # Register your models here
+admin.site.register(OneOutOfTwenty)
--- lottoGames/models.py
+++ lottoGames/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from accounts.models import Profile
 # Create your models here.
+class OneOutOfTwenty(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE),
+    score = models.PositiveBigIntegerField(default=0),
+    gameDate = models.DateTimeField(auto_now_add=True),
+    luckyNumber = models.IntegerField()
