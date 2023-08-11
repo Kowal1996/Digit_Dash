6a44529743ee6515e44b66c277e0c04163a27400
JakubJas1997 <j.j4sinski@gmail.com>
2023-08-11 15:55:03 +0200

    added games tests

--- lottoGames/tests.py
+++ lottoGames/tests.py
@@ -1,16 +1,64 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from accounts.models import Profile
+from .models import OneOutOfTwenty
+import datetime
 
-# Create your tests here.
-# class PointsAddTestCase(TestCase): # pass
-#     def setUp(self):
-#         User.objects.create(username='UserTest', email= 'test@test.pl', password='Test1234!')
-#         Profile.objects.create('city': 'TestCity',
-#             'country': 'TestCountry',
-#             'birth_date': date.today(),
-#             'accounts_balance':'0')
 
-#     def test_points_add(self):
-#         UserTest = User.objects.get(username='UserTest')
-#         UserTest1 = User.objects.get(username='UserTest1')
-#         self.assertEqual(UserTest.password, 'Test1234!')
-#         self.assertEqual(UserTest1.password, 'Test12345!')
+class GamePlayedTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username= 'Testuser',
+            email='test@test.pl',
+            password = 'Test1234!',
+            first_name='Testname'
+            )
+        self.profile = Profile.objects.create(
+            city="Testcity",
+            country="Testcountry",
+            birth_date = "1990-01-01",
+            owner=self.user
+        )
+        def create_game(self):
+            game = OneOutOfTwenty.objects.create(
+            owner = self.profile,
+            user_score = 10,
+            luckyNumber = 10
+        )
+            
+            self.assertTrue(game.luckyNumber, 10)
+            self.assertTrue(game.user_score, 10)
+            self.assertTrue(game.owner, self.profile)
+            self.assertTrue(game.gameDate, datetime.datetime.now())
+
+
+class UserGamesTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username= 'Testuser',
+            email='test@test.pl',
+            password = 'Test1234!',
+            first_name='Testname'
+            )
+        self.profile = Profile.objects.create(
+            city="Testcity",
+            country="Testcountry",
+            birth_date = "1990-01-01",
+            owner=self.user
+        )
+    def test_user_games(self):
+        game1 = OneOutOfTwenty.objects.create(
+            owner = self.profile,
+            user_score = 8,
+            luckyNumber = 7
+        )
+        self.profile.account_balance += game1.user_score
+        game2 = OneOutOfTwenty.objects.create(
+            owner = self.profile,
+            user_score = 4,
+            luckyNumber = 8
+        )
+        self.profile.account_balance += game2.user_score
+        self.assertTrue(game1.owner, self.profile)
+        self.assertTrue(game2.owner, self.profile)
+        self.assertTrue(self.profile.account_balance, 12)
