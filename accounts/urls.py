547eb2c5082b9cda68aa65db9232342489491c37
kamila <czajkowska.kamila@wp.pl>
2023-08-12 12:41:54 +0200

    Added reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change_form.html', success_url = '/password_change/done'), name='password_change', ),
     path('password_change/done', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
     path('registrationSuccessfull/', views.registrationSuccessfull, name='registrationSuccessfull'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
